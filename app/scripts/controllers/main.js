a8ff1fe940a3a7ceff34737d7c40e99e011bbf8c
phil <phil@phil-HP-Compaq-6830s.(none)>
2013-07-23 05:15:16 +0200

    Init with Yeo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('YOANGULARApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+ //   function PhoneListCtrl($scope) {
+  $scope.phones = [
+    {"name": "Nexus S",
+     "snippet": "Fast just got faster with Nexus S."},
+    {"name": "Motorola XOOM™ with Wi-Fi",
+     "snippet": "The Next, Next Generation tablet."},
+    {"name": "MOTOROLA XOOM™",
+     "snippet": "The Next, Next Generation tablet."}
+  ];
+//}
+  });
